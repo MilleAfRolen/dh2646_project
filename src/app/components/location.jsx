b6142ddf31525223f404ac6dc2a996b858ff7e95
Mille af Rolén <milleafrolen@Milles-MBP.lan>
2023-11-29 23:41:37 +0100

    added intervals to the api

--- src/app/components/location.jsx
+++ src/app/components/location.jsx
@@ -7,22 +7,35 @@ export default function Location() {
   const [weather, setWeather] = useState(null);
   const [loading, setLoading] = useState(true);
 
-  useEffect(() => {
-    getCurrentLocation().then((location) => {
+  async function fetchWeather() {
+    try {
+      const location = await getCurrentLocation();
       const { lat, lon } = location;
       const url = `${BASE_URL}lat=${lat}&lon=${lon}&units=metric&appid=${API_KEY}`;
-      fetch(url)
-        .then((response) => response.json())
-        .then((data) => {
-          console.log(data);
-          setWeather(data);
-          setLoading(false); // Set loading to false after API response
-        });
-    });
+      const response = await fetch(url);
+      const data = await response.json();
+      console.log(data);
+      setWeather(data);
+      setLoading(false); // Set loading to false after API response
+    } catch (error) {
+      console.error(error);
+    }
+  }
+
+  useEffect(() => {
+    fetchWeather();
+    const interval = setInterval(fetchWeather, 60000); // Update every minute (60000 milliseconds)
+
+    return () => clearInterval(interval); // Clean up the interval on component unmount
   }, []);
 
-  function getCurrentLocation() {
-    return fetch("http://ip-api.com/json/").then((response) => response.json());
+  async function getCurrentLocation() {
+    try {
+      const response = await fetch("http://ip-api.com/json/");
+      return await response.json();
+    } catch (error) {
+      console.error(error);
+    }
   }
 
   return (
