817d4b9bac1f2ba2844baf31e62485637df253d5
Mille af Rolén <milleafrolen@Milles-MBP.lan>
2023-11-29 23:30:50 +0100

    Played around with more css and added an icon

--- src/app/components/location.jsx
+++ src/app/components/location.jsx
@@ -1,40 +1,52 @@
 "use client";
 
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { BASE_URL, API_KEY } from "../../weatherApiConfig";
 
 export default function Location() {
   const [weather, setWeather] = useState(null);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    getCurrentLocation().then((location) => {
+      const { lat, lon } = location;
+      const url = `${BASE_URL}lat=${lat}&lon=${lon}&units=metric&appid=${API_KEY}`;
+      fetch(url)
+        .then((response) => response.json())
+        .then((data) => {
+          console.log(data);
+          setWeather(data);
+          setLoading(false); // Set loading to false after API response
+        });
+    });
+  }, []);
+
   function getCurrentLocation() {
     return fetch("http://ip-api.com/json/").then((response) => response.json());
   }
 
-  function getWeather() {
-    getCurrentLocation().then((location) => {
-      console.log(location);
-      const { lat, lon } = location;
-      const url = `${BASE_URL}lat=${lat}&lon=${lon}&appid=${API_KEY}`;
-      fetch(url)
-        .then((response) => response.json())
-        .then((data) => {
-          setWeather(data);
-        });
-    });
-  }
   return (
-    <div>
-      <button
-        className="border border-solid rounded border-2 bg-blue-700 hover:bg-blue-500 text-white font-bold py-2 px-4"
-        onClick={getWeather}
-      >
-        Get Current Location
-      </button>
-      {weather && (
-        <div>
-          <h2>Weather Data</h2>
-          <pre>{JSON.stringify(weather["weather"], null, 2)}</pre>
+    <div className="text-center">
+      {loading ? (
+        <p>Loading...</p> // Display loading state while waiting for API response
+      ) : (
+        <div className="my-8">
+          {weather && (
+            <div>
+              <h3>Main Weather Information</h3>
+              <p>Current Temperature: {weather["main"]["temp"]}°C</p>
+              <p>Humidity: {weather["main"]["humidity"]}%</p>
+              {/* Add more properties from the 'main' object as needed */}
+            </div>
+          )}
         </div>
       )}
+      <button
+        className="bg-blue-700 hover:bg-blue-900 text-white font-bold py-2 px-4 mx-2 rounded"
+        onClick={() => {}}
+      >
+        Generate Content
+      </button>
     </div>
   );
 }
--- src/app/components/navbar.jsx
+++ src/app/components/navbar.jsx
@@ -3,16 +3,21 @@ import Link from "next/link";
 
 export default function Navbar() {
   return (
-    <div className="flex justify-between items-center bg-gray-800 p-4">
+    <div className="flex justify-between items-center bg-gray-800 p-4 fixed w-full z-50">
       <div className="text-white font-bold text-xl">
         <Link href="#">Weather App</Link>
       </div>
-      <div className="flex">
-        <a href="">
-          <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+      <div className="flex justify-around">
+        <Link href="">
+          <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 mx-2 rounded">
             Sign In
           </button>
-        </a>
+        </Link>
+        <Link href="">
+          <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 mx-2 rounded">
+            Sing up
+          </button>
+        </Link>
       </div>
     </div>
   );
