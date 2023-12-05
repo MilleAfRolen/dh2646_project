d6fcfb8dfa866591512ef1d0d5c2bef91d43b567
Mille af Rolén <milleafrolen@Milles-MacBook-Pro.local>
2023-12-05 21:08:29 +0100

    fixed routing

--- src/app/(views)/locationView.jsx
+++ src/app/(views)/locationView.jsx
@@ -1,3 +1,5 @@
+import Link from "next/link";
+
 export default function LocationView({ weather }) {
   return (
     <div className="text-center">
@@ -12,9 +14,11 @@ export default function LocationView({ weather }) {
           </div>
         )}
       </div>
-      <button className="bg-blue-700 hover:bg-blue-900 text-white font-bold py-2 px-4 mx-2 rounded">
-        Generate Content
-      </button>
+      <Link href="/animelist">
+        <button className="bg-blue-700 hover:bg-blue-900 text-white font-bold py-2 px-4 mx-2 rounded">
+          Generate Content
+        </button>
+      </Link>
     </div>
   );
 }
