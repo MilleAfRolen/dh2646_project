654b35bccfdb253f7bad3ed220d93e7655927201
Mille af Rolén <milleafrolen@Milles-MacBook-Pro.local>
2023-12-08 14:09:59 +0100

    Moa needs update

---
+++ src/app/(views)/animeDetails/page.jsx
@@ -0,0 +1,9 @@
+import AnimeDetailsPresenter from "@/app/(presenters)/animeDetailsPresenter";
+
+export default function AnimeDetails() {
+  return (
+    <div>
+      <AnimeDetailsPresenter />
+    </div>
+  );
+}
