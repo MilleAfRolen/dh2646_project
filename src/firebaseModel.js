6d6ffe1cd854b0b04a2103128ae376ccc04bb217
Anton Rådberg <aradberg@kth.se>
2023-12-08 15:10:30 +0100

    deployed

--- src/firebaseModel.js
+++ src/firebaseModel.js
@@ -1,10 +1,13 @@
 // Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
+import { initializeApp, getApps } from "firebase/app";
 import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
-import firebaseConfig from "./firebaseConfig";
+import firebaseConfig from "./firebaseConfig.js";
 // TODO: Add SDKs for Firebase products that you want to use
 
-const app = initializeApp(firebaseConfig);
-const auth = getAuth(app);
-const db = getFirestore(app);
+// const app = initializeApp(firebaseConfig);
+// const db = getDatabase(app);
+// const auth = getAuth(app);
+
+let firebase_app = getApps().length === 0 ? initializeApp(firebaseConfig) : getApps()[0];
+export default firebase_app;
