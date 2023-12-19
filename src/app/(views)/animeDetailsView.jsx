bd11f2a81a710d8337b563fd18d812979489da64
Mille af Rolén <milleafrolen@Milles-MacBook-Pro.local>
2023-12-19 15:13:56 +0100

    fixed errors

--- src/app/(views)/animeDetailsView.jsx
+++ src/app/(views)/animeDetailsView.jsx
@@ -13,39 +13,56 @@ export default function AnimeDetailsView({
     return (
       <div>
         <div>
-            <div>
-                <p className="text-4xl font-bold pt-4">{title}</p>    
-            </div>
-            <div className="border-solid border-black rounded-2xl border-2 py-5 px-5 my-4">
-                    <p> <strong>Type:</strong> {type}</p>
-                    <p> <strong>Source:</strong> {source}</p>
-                    <p> <strong>Episodes:</strong> {episodes}</p>
-                    <p> <strong>Status:</strong> {status}</p>
-                    <p> <strong>Genres:</strong> </p>
-                </div>
-            <div className="my-5 border-solid border-black rounded-2xl border-2 py-5 px-5">
-                <p>{description}</p>
-            </div>
+          <div>
+            <p className="text-4xl font-bold pt-4">{title}</p>
+          </div>
+          <div className="border-solid border-black rounded-2xl border-2 py-5 px-5 my-4">
+            <p>
+              {" "}
+              <strong>Type:</strong> {type}
+            </p>
+            <p>
+              {" "}
+              <strong>Source:</strong> {source}
+            </p>
+            <p>
+              {" "}
+              <strong>Episodes:</strong> {episodes}
+            </p>
+            <p>
+              {" "}
+              <strong>Status:</strong> {status}
+            </p>
+            <p>
+              {" "}
+              <strong>Genres:</strong>{" "}
+            </p>
+          </div>
+          <div className="my-5 border-solid border-black rounded-2xl border-2 py-5 px-5">
+            <p>{description}</p>
+          </div>
         </div>
-            
-    )}
+      </div>
+    );
+  }
 
-    function renderAnimeImage() {
-        return (
-            <div>
-                <Image className="h-full w-full object-cover rounded-3xl border border-black my-5"
-                  src={image}
-                  height={0}
-                  width={0}
-                  sizes="100vw"
-                  priority={true}
-                  alt={title}
-                />
-            </div>
-        );
-    }
+  function renderAnimeImage() {
+    return (
+      <div>
+        <Image
+          className="h-full w-full object-cover rounded-3xl border border-black my-5"
+          src={image}
+          height={0}
+          width={0}
+          sizes="100vw"
+          priority={true}
+          alt={title}
+        />
+      </div>
+    );
+  }
 
-    //aired. premiered, producors, studio, genre, theme, duration
+  //aired. premiered, producors, studio, genre, theme, duration
 
   return (
     <div className="py-2 px-20 z-10 bg-[#e5e5e5] flex flex-row">
