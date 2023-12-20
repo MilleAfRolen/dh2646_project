7b12c7d2935bf12898359b2521bd2600d88d1b56
Mille af Rolén <milleafrolen@Milles-MacBook-Pro.local>
2023-12-20 15:17:18 +0100

    minor changes

--- src/firebaseModel.js
+++ src/firebaseModel.js
@@ -1,7 +1,12 @@
-'use client'
+"use client";
 import { initializeApp, getApps } from "firebase/app";
-import { createUserWithEmailAndPassword, signInWithEmailAndPassword, getAuth, signOut } from "firebase/auth";
-import { useState} from "react";
+import {
+  createUserWithEmailAndPassword,
+  signInWithEmailAndPassword,
+  getAuth,
+  signOut,
+} from "firebase/auth";
+import { useState } from "react";
 
 export const firebaseConfig = {
   apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
@@ -14,56 +19,67 @@ export const firebaseConfig = {
   measurementId: process.env.NEXT_PUBLIC_MEASUREMENT_ID,
 };
 
-let firebase_app = getApps().length === 0 ? initializeApp(firebaseConfig) : getApps()[0];
+let firebase_app =
+  getApps().length === 0 ? initializeApp(firebaseConfig) : getApps()[0];
 export const auth = getAuth(firebase_app);
 
 export default function firebaseModel() {
-    const [email, setEmail] = useState('');
-    const [password, setPassword] = useState('');
-    const [loading, setLoading] = useState(false);
-    const [error, setError] = useState(null);
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(null);
 
-    function setTheEmail(newEmail) {
-      setEmail(newEmail);
+  function setTheEmail(newEmail) {
+    setEmail(newEmail);
+  }
+
+  function setThePassword(newPass) {
+    setPassword(newPass);
+  }
+
+  const handleSignUp = async () => {
+    try {
+      setLoading(true);
+      const result = await createUserWithEmailAndPassword(
+        auth,
+        email,
+        password
+      );
+      console.log({ result });
+    } catch (error) {
+      console.log("Sign-up error: ", error.message);
+      alert(error.message);
+      setError(error.message);
+    } finally {
+      setLoading(false);
     }
+  };
 
-    function setThePassword(newPass) {
-      setPassword(newPass);
+  const handleSignIn = async () => {
+    try {
+      setLoading(true);
+      const result = await signInWithEmailAndPassword(auth, email, password);
+      console.log({ result });
+    } catch (error) {
+      console.log("Sign-in error: ", error.message);
+      alert(error.message);
+      setError(error.message);
+    } finally {
+      setLoading(false);
     }
+  };
 
-    const handleSignUp = async () => {
-      try {
-        setLoading(true);
-        const result = await createUserWithEmailAndPassword(auth, email, password);
-        console.log({ result });
+  const logout = () => {
+    signOut(auth);
+  };
 
-      } catch (error) {
-        console.log("Sign-up error: ", error.message);
-        alert(error.message);
-        setError(error.message);
-      } finally {
-        setLoading(false);
-      }
-    };
-
-    const handleSignIn = async () => {
-      try {
-        setLoading(true);
-        const result = await signInWithEmailAndPassword(auth, email, password);
-        console.log({ result });
-
-      } catch (error) {
-        console.log("Sign-in error: ", error.message);
-        alert(error.message);
-        setError(error.message);
-      } finally {
-        setLoading(false);
-      }
-    };
-
-    const logout = () => {
-      signOut(auth);
-    };
-
-  return { setTheEmail, setThePassword, handleSignUp, handleSignIn, logout, loading, error };
+  return {
+    setTheEmail,
+    setThePassword,
+    handleSignUp,
+    handleSignIn,
+    logout,
+    loading,
+    error,
+  };
 }
